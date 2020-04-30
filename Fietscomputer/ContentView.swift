91277acbdb5dc3b295fcbd39ebee092f688c6125
Grigory Avdyushin <avdyushin.g@gmail.com>
2020-04-30 18:10:12 +0200

    Initial Commit

---
+++ Fietscomputer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Fietscomputer
+//
+//  Created by Grigory Avdyushin on 30/04/2020.
+//  Copyright © 2020 Grigory Avdyushin. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
