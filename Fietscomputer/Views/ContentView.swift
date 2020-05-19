bab770e8c797fc5e4efa0618dcbc8cf81878b2d2
Grigory Avdyushin <avdyushin.g@gmail.com>
2020-05-19 20:44:48 +0200

    Slider view added

--- Fietscomputer/Views/ContentView.swift
+++ Fietscomputer/Views/ContentView.swift
@@ -7,35 +7,54 @@
 //
 
 import SwiftUI
-import Combine
 
 struct ContentView: View {
 
     @ObservedObject var viewModel: ContentViewModel
-    @State var mapHeight: CGFloat = 0
+    @ObservedObject var position = SliderPosition()
 
     var body: some View {
         GeometryReader { geometry in
-            VStack {
-                MapView(viewModel: self.viewModel.mapViewModel)
-                    .frame(minHeight: 0, idealHeight: self.mapHeight, maxHeight: .infinity)
-                PullView(height: self.$mapHeight) {
+            ZStack {
+                VStack {
+                    VStack {
+                        MapView(viewModel: self.viewModel.mapViewModel)
+                            .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity)
+                        VStack {
+                            SpeedView(viewModel: self.viewModel.speedViewModel).frame(minHeight: 0, maxHeight: .infinity)
+                            HStack {
+                                GaugeView(viewModel: self.viewModel.durationViewModel).frame(minWidth: 0, maxWidth: .infinity)
+                                GaugeView(viewModel: self.viewModel.distanceViewModel).frame(minWidth: 0, maxWidth: .infinity)
+                            }.padding(20)
+                            Button(action: self.viewModel.startPauseRide) {
+                                Text(self.viewModel.buttonTitle)
+                            }
+                            .foregroundColor(.green)
+                            .buttonStyle(ActionButtonStyle())
+                            .padding(EdgeInsets(top: 0, leading: 0, bottom: geometry.safeAreaInsets.bottom, trailing: 0))
+                            .background(Color.green)
+                        }
+                        .frame(height: geometry.size.height * 0.5 - 10 /* ? */ - self.position.value)
+                        .background(Color(UIColor.systemBackground))
+                    }
+                }
+                SliderView(position: self.position, geometry: geometry, range: 0.3...0.8) {
                     Group {
-                        SpeedView(viewModel: self.viewModel.speedViewModel).frame(minHeight: 0, maxHeight: .infinity)
-                        HStack {
-                            GaugeView(viewModel: self.viewModel.durationViewModel).frame(minWidth: 0, maxWidth: .infinity)
-                            GaugeView(viewModel: self.viewModel.distanceViewModel).frame(minWidth: 0, maxWidth: .infinity)
-                        }.padding(20)
-                    }.background(Color.white)
+                        ZStack(alignment: .bottom) {
+                            Rectangle()
+                                .foregroundColor(Color(UIColor.systemBackground))
+                                .frame(minWidth:0, maxWidth: .infinity, minHeight: 6, maxHeight: 6)
+                                .shadow(color: Color.black.opacity(0.05), radius: 1, x: 0, y: -5)
+                            RoundedRectangle(cornerRadius: 5)
+                                .fill()
+                                .frame(width: 48, height: 3)
+                        }
+                    }
+                    .foregroundColor(Color(UIColor.separator))
+                    .frame(minWidth: 0, maxWidth: .infinity)
+                    .background(Color.clear)
                 }
-                Text("\(self.mapHeight)")
-                Button(action: self.viewModel.startPauseRide) {
-                    Text(self.viewModel.buttonTitle)
-                }
-                .foregroundColor(.green)
-                .buttonStyle(ActionButtonStyle())
-                .padding(EdgeInsets(top: 0, leading: 0, bottom: geometry.safeAreaInsets.bottom, trailing: 0))
             }
-        }.edgesIgnoringSafeArea(.bottom)
+        }.edgesIgnoringSafeArea([.horizontal, .bottom])
     }
 }
---
+++ Fietscomputer/Views/SliderView.swift
@@ -0,0 +1,46 @@
+//
+//  PullView.swift
+//  Fietscomputer
+//
+//  Created by Grigory Avdyushin on 18/05/2020.
+//  Copyright © 2020 Grigory Avdyushin. All rights reserved.
+//
+
+import SwiftUI
+
+class SliderPosition: ObservableObject {
+    @Published var value: CGFloat = 0
+    @Published var previous: CGFloat = 0
+}
+
+struct SliderView<Content: View>: View {
+
+    @ObservedObject var position: SliderPosition
+    var geometry: GeometryProxy
+    var range: ClosedRange<CGFloat>
+
+    let content: () -> Content
+
+    var body: some View {
+        VStack {
+            content()
+        }
+        .offset(y: position.value)
+        .gesture(
+            DragGesture()
+                .onChanged(onDragChanged(gesture:))
+                .onEnded(onDragEnded(gesture:))
+        )
+    }
+
+    fileprivate func onDragChanged(gesture: DragGesture.Value) {
+        let height = position.previous + gesture.translation.height
+        let high = geometry.size.height * (0.5 - range.lowerBound)
+        let low = geometry.size.height * (0.5 - range.upperBound)
+        position.value = max(low, min(high, height))
+    }
+
+    fileprivate func onDragEnded(gesture: DragGesture.Value) {
+        position.previous = position.value
+    }
+}
