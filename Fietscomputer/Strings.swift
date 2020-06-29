94741988478d57591f82d3484fe180cdb26902de
Grigory Avdyushin <avdyushin.g@gmail.com>
2020-06-29 17:00:13 +0200

    Update strings

--- Fietscomputer/Strings.swift
+++ Fietscomputer/Strings.swift
@@ -13,11 +13,16 @@ enum Strings {
     @Localizable static var avg_speed = "avg_speed"
     @Localizable static var max_speed = "max_speed"
     @Localizable static var elevation_gain = "elevation_gain"
+    @Localizable static var elevation = "elevation"
+    @Localizable static var speed = "speed"
     @Localizable static var avg_power = "avg_power"
     @Localizable static var energy_output = "energy_output"
     @Localizable static var weight_loss = "weight_loss"
     @Localizable static var remove_ride = "remove_ride"
     @Localizable static var remove_ride_message = "remove_ride_message"
     @Localizable static var confirm_delete = "confirm_delete"
+    @Localizable static var import_gpx_message = "import_gpx_message"
+    @Localizable static var yes = "yes"
+    @Localizable static var no = "no"
 }
 
